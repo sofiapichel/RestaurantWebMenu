c7459b26a0bd1bc7e815073ae486a5fa82c23b06
sofiapichel <sofiapichel1@gmail.com>
2022-09-15 12:54:19 -0300

    First commit

---
+++ Restaurant.Models/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Models
+{
+    [Table(name: "Restaurants")]
+    public class Restaurant
+    {
+
+    }
+}

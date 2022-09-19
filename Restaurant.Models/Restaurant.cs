85b280c62a276387469e1870ea4e9a57fc74166c
sofiapichel <sofiapichel1@gmail.com>
2022-09-19 10:26:15 -0300

    Agregado de section INFO

--- Restaurant.Models/Restaurant.cs
+++ Restaurant.Models/Restaurant.cs
@@ -7,6 +7,8 @@ namespace Restaurant.Models
     [Table(name: "Restaurants")]
     public class Restaurant
     {
+        public int Id { get; set; }
 
+        public string Nombre { get; set; }
     }
 }
